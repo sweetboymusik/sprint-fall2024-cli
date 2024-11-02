9fa64d5ee92399bddfae15866678f10cf5d99313
sweetboymusik <ellbutt@gmail.com>
2024-11-02 20:31:51 -0230

    get all cities test fetch to api successful

--- src/main/java/http/client/RESTClient.java
+++ src/main/java/http/client/RESTClient.java
@@ -1,31 +1,56 @@
 package http.client;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import domain.City;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RESTClient {
     private String serverURL;
     private HttpClient client;
 
-    public String getResponseFromHTTPRequest() {
-        String responseBody = "";
+    public HttpResponse<String> httpSender(HttpRequest request) throws IOException, InterruptedException {
+        HttpResponse<String> response = getClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() != 200) {
+            System.out.println("Error Status Code: " + response.statusCode());
+        }
+
+        return response;
+    }
+
+    public List<City> getAllCities() {
+        List<City> cities = new ArrayList<>();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(serverURL)).build();
 
         try {
-            HttpResponse<String> response = getClient().send(request, HttpResponse.BodyHandlers.ofString());
-
-            if (response.statusCode() != 200) {
-                System.out.println("Error Status Code: " + response.statusCode());
-            }
-            responseBody = response.body();
+            HttpResponse<String> response = httpSender(request);
+            cities = buildCityListFromResponse(response.body());
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
 
-        return responseBody;
+        return cities;
+    }
+
+    public List<City> buildCityListFromResponse(String response) throws JsonProcessingException {
+        List<City> cities = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        cities = mapper.readValue(response, new TypeReference<List<City>>() {
+        });
+
+        return cities;
+
     }
 
     public HttpClient getClient() {
